c8dcd09d61ec53a71cfcf6f01d1d8b187892e0e9
ayu-22 <ayush.singh.222k@gmail.com>
2018-12-23 01:35:14 +0530

    new code

--- app/src/main/java/com/example/tidu/attendancemanager2/MainActivity.java
+++ app/src/main/java/com/example/tidu/attendancemanager2/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.tidu.attendancemanager2;
 
 import android.app.AlertDialog;
 import android.database.Cursor;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.support.design.widget.NavigationView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnAddData;
     Button btnviewAll;
     Button btnDelete;
-
+    private DrawerLayout DrawerLayout1;
     Button btnviewUpdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +43,107 @@ public class MainActivity extends AppCompatActivity {
         viewAll();
         UpdateData();
         DeleteData();
+
+        DrawerLayout1 = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        DrawerLayout1.closeDrawers();
+
+                        if(menuItem.getTitle().equals("Add Subject"))   //when "Add Subject" item is selected
+                        {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                            LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+                            View view = inflater.inflate(R.layout.dialog_add,null);
+
+                            final EditText sub = (EditText)view.findViewById(R.id.subjectAdd);
+                            final EditText min =  (EditText)view.findViewById(R.id.minAdd);
+                            Button add_Btn =  (Button)view.findViewById(R.id.AddBtn);
+                            Button cancel_Btna = (Button)view.findViewById(R.id.CancelBtna);
+                            final String subject = sub.getText().toString();//stores the value of subject typed
+                            final String minimum = min.getText().toString();//stores the minimum attendence
+                            builder.setView(view);
+                            final AlertDialog add_dialog = builder.create();
+                            add_dialog.show(); // add dialogbox is shown
+
+                            add_Btn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {                      //intructions to be performed if add button is clicked
+
+                                    boolean isinserted=myDb.insertData(subject,minimum,"0","0","0");//inserts data
+                                    if(isinserted)
+                                        Toast.makeText(MainActivity.this,"Data added",Toast.LENGTH_SHORT).show();
+                                    else
+                                        Toast.makeText(MainActivity.this,"Some Error Occured",Toast.LENGTH_SHORT).show();
+
+                                    add_dialog.dismiss();
+                                }
+                            });
+
+                            cancel_Btna.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {    ////intructions to be performed if cancel button is clicked
+                                    add_dialog.dismiss();
+                                }
+                            });
+
+
+
+
+                        }
+                        else if (menuItem.getTitle().equals("Delete Subject"))     //when "Delete Subject" is selected
+                        {
+                            AlertDialog.Builder builder2 = new AlertDialog.Builder(MainActivity.this);
+
+                            LayoutInflater inflater2 = MainActivity.this.getLayoutInflater();
+                            View view2 = inflater2.inflate(R.layout.dialog_delete,null);
+
+                            final EditText deleteSub = (EditText)view2.findViewById(R.id.deleteSubject);
+
+                            Button delete_Btn =  (Button)view2.findViewById(R.id.Deletebtn);
+                            Button cancel_Btnd = (Button)view2.findViewById(R.id.Cancelbtnd);
+                            final String subject = deleteSub.getText().toString();  //stores the value of subject enter for deletion
+
+                            builder2.setView(view2);
+                            final AlertDialog add_dialog2 = builder2.create();
+                            add_dialog2.show();     //delete dialogbox is shown
+
+                            delete_Btn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {   ////intruction to be performed if delete button is clicked
+                                    Integer deletedRows = myDb.deleteData(subject);
+                                    if(deletedRows > 0)
+                                        Toast.makeText(MainActivity.this,"Data Deleted",Toast.LENGTH_LONG).show();
+                                    else
+                                        Toast.makeText(MainActivity.this,"Data not Deleted",Toast.LENGTH_LONG).show();
+
+
+                                    add_dialog2.dismiss();
+                                }
+                            });
+
+                            cancel_Btnd.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) { ////intruction to be performed if cancel button is clicked
+                                    add_dialog2.dismiss();
+                                }
+                            });
+
+                        }
+
+
+                        return true;
+                    }
+                });
+
+
     }
     public void DeleteData() {
         btnDelete.setOnClickListener(
